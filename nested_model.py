80412ad56a7c8615edef00c5d2d97330350955d3
SHRESTHsGIT <shresthshankhdhar@gmail.com>
2025-08-09 07:15:53 +0530

    pydentic

--- nested_model.py
+++ nested_model.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+# when we use one model as field in another model we call it nested model
 class Address(BaseModel):
 
     city: str
@@ -18,35 +18,35 @@ address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
 address1 = Address(**address_dict)
 
 patient_dict = {'name': 'nitish', 'gender': 'male', 'age': 35, 'address': address1}
-
+print(patient_dict)# output- {'name': 'nitish', 'gender','
 patient1 = Patient(**patient_dict)
 
-temp = patient1.model_dump(include=)
-
+temp = patient1.model_dump()
+print(temp)
+# output-{name: 'nitish..}
 print(type(temp))
+#output- <class 'dict'>
+# model_dump() converts the model to a dictionary
+
+#for json we can use model_dump_json()
+temp2= patient1.model_dump_json()
+print(temp2)
+# output- {"name": "nit
+
+temp3=patient1.model_dump_json(include={'name', 'address'})
+#now only these 2 fields will be included in the output
+print(temp3)
+# output- {"name": "nitish", "address": {"city": "gurgaon", "state": "haryana", "pin": "122001"}}
+
+
+temp4=patient1.model_dump(exclude={'address':['state']})
+#now state will be excluded from the output
+
+#other important parameter like - exclude_unset- used for ...
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# benefits of using nested models:
 # Better organization of related data (e.g., vitals, address, insurance)
 
 # Reusability: Use Vitals in multiple models (e.g., Patient, MedicalRecord)
